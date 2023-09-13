ca9632c2c090e526881c6450b6df6af1e9a2866d
Augustin Chassine <augustin.chassine@mabsilico.com>
2023-09-13 22:19:12 +0200

    refactor: elements demo + URLchain POC

---
+++ src/routes/components/simple-elements/DemoURLChain.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	// replace "$lib/" with "molstar-svelte/" in your project:
+	import StructureURLChain from '$lib/elements/StructureURLChain.svelte';
+	import MolstarWrapper from '$lib/wrappers/SimpleWrapper.svelte';
+	import StructuresURLsList from './StructuresURLsList.svelte';
+	export let structuresURLs = [
+		{ url: 'https://files.rcsb.org/view/7U4A.pdb', type: 'pdb', chainId: 'A' },
+		{ url: 'https://files.rcsb.org/view/7YLB.pdb', type: 'pdb', chainId: 'A' },
+		{ url: 'https://files.rcsb.org/view/7YUB.cif', type: 'mmcif', chainId: 'A' },
+		{
+			url: 'https://alphafold.ebi.ac.uk/files/AF-P00533-F1-model_v4.cif',
+			type: 'mmcif',
+			chainId: 'A'
+		}
+	];
+	let selectedStructuresURLs = [...structuresURLs];
+</script>
+
+<p class="text-xs">
+	Selected: <span class="text-violet-500"
+		>{selectedStructuresURLs.map((e) => e.url).join(', ')}</span
+	>
+</p>
+<MolstarWrapper class="h-96">
+	<svelte:fragment slot="elements">
+		{#each selectedStructuresURLs as structureURL (`${structureURL.url}-${structureURL.type}`)}
+			<StructureURLChain
+				url={structureURL.url}
+				type={structureURL.type}
+				chainId={structureURL.chainId}
+			/>
+		{/each}
+	</svelte:fragment>
+</MolstarWrapper>
+{#if structuresURLs.length > 1}
+	<StructuresURLsList {structuresURLs} bind:selectedStructuresURLs />
+{/if}
