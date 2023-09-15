4fce9dbba8c3356bb6175e4b729a49617847df14
Augustin Chassine <augustin.chassine@mabsilico.com>
2023-09-15 19:08:41 +0200

    feat: add demo page for "controls" components

---
+++ src/routes/components/simple-controls/DemoControls.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import StructureURL from '$lib/elements/StructureURL.svelte';
+	import HighlightInfo from '$lib/controls/HighlightInfo.svelte';
+	import MolstarWrapper from '$lib/wrappers/SimpleWrapper.svelte';
+
+	export let structuresURLs = [
+		{ url: 'https://files.rcsb.org/view/7YUB.cif', type: 'mmcif' },
+		{ url: 'https://alphafold.ebi.ac.uk/files/AF-P00533-F1-model_v4.cif', type: 'mmcif' }
+	];
+	let selectedStructuresURLs = [...structuresURLs];
+</script>
+
+<MolstarWrapper class="h-96 relative">
+	<svelte:fragment slot="inside">
+		{#each selectedStructuresURLs as structureURL (`${structureURL.url}-${structureURL.type}`)}
+			<StructureURL url={structureURL.url} type={structureURL.type} />
+		{/each}
+		<HighlightInfo />
+	</svelte:fragment>
+	<svelte:fragment slot="outside">
+		<div>
+			Displaying:
+			{#each selectedStructuresURLs as structureURL (`${structureURL.url}-${structureURL.type}`)}
+				<p class="text-xs"><span>{structureURL.url}</span> <span>({structureURL.type})</span></p>
+			{/each}
+		</div>
+	</svelte:fragment>
+</MolstarWrapper>
